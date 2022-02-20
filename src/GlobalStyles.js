dbd0088e128f3f6311847d1dde5db354a36cb4bc
seulgi <dltmfrl600@gmail.com>
2022-02-20 22:35:04 +0900

    Chore: css > scss

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
-import "css/fonts/font.module.css";
+import "css/fonts/font.module.scss";
 
 const GlobalStyles = createGlobalStyle` 
 
